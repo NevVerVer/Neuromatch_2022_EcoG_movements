06faa64073cbea680f0ea80cfcb1ff9fb444a974
vagechirkov <vagechirkov@gmail.com>
2022-07-28 09:46:13 +0200

    add reproducible notebook

--- models/reach_ae/utils.py
+++ models/reach_ae/utils.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import numpy as np
 import pytorch_lightning as pl
@@ -18,7 +19,13 @@ def train_model(model,
                 n_epochs=20,
                 batch_size=20,
                 add_to_log_name=None,
-                accumulate_grad_batches=None):
+                accumulate_grad_batches=None,
+                seed=42):
+    set_seed(seed=seed)
+    pl.seed_everything(seed, True)
+    g_seed = torch.Generator()
+    g_seed.manual_seed(seed)
+
     if accumulate_grad_batches is None:
         accumulate_grad_batches = {0: 8, 4: 4, 8: 1}
     if add_to_log_name is None:
@@ -44,7 +51,7 @@ def train_model(model,
             ModelCheckpoint(
                 save_top_k=2,
                 dirpath=os.path.join(logger.log_dir, "checkpoints"),
-                monitor="val_loss",
+                monitor="validation_loss",
                 save_last=True),
         ],
         accumulate_grad_batches=accumulate_grad_batches
@@ -53,12 +60,17 @@ def train_model(model,
     # Perform training
     trainer.fit(
         model,
-        DataLoader(X_train, batch_size=batch_size, shuffle=True),
-        DataLoader(X_val, batch_size=batch_size, shuffle=False)
+        DataLoader(X_train, batch_size=batch_size, shuffle=True,
+                   worker_init_fn=seed_worker, generator=g_seed,
+                   pin_memory=True),
+        DataLoader(X_val, batch_size=batch_size, shuffle=False,
+                   worker_init_fn=seed_worker, generator=g_seed,
+                   pin_memory=True)
     )
 
     # Perform evaluation
-    trainer.test(model, DataLoader(X_test, shuffle=False))
+    trainer.test(model, DataLoader(
+        X_test, shuffle=False, worker_init_fn=seed_worker, generator=g_seed))
 
 
 def plot_reach(ax, data, event, plot_ticks_and_labels=True, plot_line=True):
@@ -183,3 +195,52 @@ def plot_examples_from_class(labels, data, n_ex=5):
                        plot_line=False)
         ax[i].set_title(f'Cluster {l}')
     plt.show()
+
+
+def set_seed(seed=None, seed_torch=True):
+    """
+    Function that controls randomness. NumPy and random modules must be imported.
+
+    Args:
+      seed : Integer
+        A non-negative integer that defines the random state. Default is `None`.
+      seed_torch : Boolean
+        If `True` sets the random seed for pytorch tensors, so pytorch module
+        must be imported. Default is `True`.
+
+    Returns:
+      Nothing.
+    """
+    if seed is None:
+        seed = np.random.choice(2 ** 32)
+    random.seed(seed)
+    np.random.seed(seed)
+    if seed_torch:
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.cuda.manual_seed(seed)
+        # torch.mps.seed(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    print(f'Random seed {seed} has been set.')
+
+
+# In case that `DataLoader` is used
+def seed_worker(worker_id):
+    """
+    DataLoader will reseed workers following randomness in
+    multi-process data loading algorithm.
+
+    Args:
+      worker_id: integer
+        ID of subprocess to seed. 0 means that
+        the data will be loaded in the main process
+        Refer: https://pytorch.org/docs/stable/data.html#data-loading-randomness for more details
+
+    Returns:
+      Nothing
+    """
+    worker_seed = torch.initial_seed() % 2 ** 32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
